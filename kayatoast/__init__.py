c6a59fa17944eceb7dde7a707f640a043d99cadb
Xavier Chen <xaviernehc@gmail.com>
2025-06-07 14:40:09 +0800

    add flask

--- kayatoast/__init__.py
+++ kayatoast/__init__.py
@@ -1,2 +1,13 @@
 from logging.config import dictConfig
+
+from flask import Flask
+
 from .log_conf import d
+
+def create_app() -> Flask:
+    server = Flask(__name__, instance_relative_config=True)
+
+    # Configure logging
+    dictConfig(d)
+
+    return server
