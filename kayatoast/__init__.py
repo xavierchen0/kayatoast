e43d13c203ce56605ac89dea939d10127542e85e
Xavier Chen <xaviernehc@gmail.com>
2025-06-07 14:54:05 +0800

    add postgres support

--- kayatoast/__init__.py
+++ kayatoast/__init__.py
@@ -1,13 +1,30 @@
+import os
 from logging.config import dictConfig
 
+from dotenv import load_dotenv
 from flask import Flask
 
 from .log_conf import d
+from .models import db
+
+load_dotenv()
+
 
 def create_app() -> Flask:
     server = Flask(__name__, instance_relative_config=True)
 
+    # Configure Flask server
+    server.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    server.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+
     # Configure logging
     dictConfig(d)
+
+    # Initalise db
+    db.init_app(server)
+
+    with server.app_context():
+        # Create tables
+        db.create_all()
 
     return server
---
+++ kayatoast/models.py
@@ -0,0 +1,12 @@
+import datetime
+from dataclasses import dataclass
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
